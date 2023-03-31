473eedd84f456c7e1336b6f403a670e0418e82b6
mika.urakawa <awakaru0122@gmail.com>
2023-03-31 22:15:41 +0900

    feat: request app store connect api

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,20 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    let session = Session()
+
+    // TODO: App Store Connect API をリクエストする
+    let request = GetAppSubmissionsRequest(appId: "1673161138")  // TopicGen
+    session.send(request) { result in
+        switch result {
+        case let .success(response):
+            // TODO: レスポンスを次のリクエストに使用する
+            print(response)
+        case let .failure(error):
+            print(error)
+        }
+    }
+
     app.get { req async in
         "It works!"
     }

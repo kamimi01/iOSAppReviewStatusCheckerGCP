c9a1e6099db6d5b3f294ca8021431263c62e11f6
mika.urakawa <awakaru0122@gmail.com>
2023-04-19 07:54:28 +0900

    refactor: create app store connect controller

---
+++ Sources/App/Controllers/AppStoreConnectController.swift
@@ -0,0 +1,59 @@
+//
+//  File.swift
+//  
+//
+//  Created by mikaurakawa on 2023/04/19.
+//
+
+import Foundation
+import Vapor
+import JWT
+
+enum AppStoreConnectRequestError: Error {
+    case cannotGenerateJWT
+}
+
+/// App Store Connect へのリクエストを受け、レスポンスを返す責務
+class AppStoreConnectController {
+    private let app: Application
+    private let req: Vapor.Request
+    private let appIDs: [String]
+
+    init(app: Application, req: Vapor.Request, appIDs: [String]) {
+        self.app = app
+        self.req = req
+        self.appIDs = appIDs
+    }
+
+    func requestReviewSubmission() async throws -> GetAppSubmissionsRequest.Response {
+        guard let jwt = generateJWT() else { throw AppStoreConnectRequestError.cannotGenerateJWT }
+
+        let session = Session()
+        let request = GetAppSubmissionsRequest(appId: appIDs.first, token: jwt)
+        let result = try await session.send(request)
+
+        return result
+    }
+
+    /// JWT を生成
+    private func generateJWT() -> String? {
+        let privateKey = privateKey
+
+        let payload = Payload(
+            issuer: .init(value: issuerID),
+            issuedAtClaim: .init(value: Date()),
+            expiration: .init(value: Calendar.current.date(byAdding: .minute, value: 20, to: Date()) ?? Date()),
+            audience: .init(value: ["appstoreconnect-v1"])
+        )
+
+        do {
+            let key = try ECDSAKey.private(pem: privateKey)
+            app.jwt.signers.use(.es256(key: key))
+            let token = try req.jwt.sign(payload, kid: keyID)
+            return token
+        } catch {
+            print("failed to generate jwt")
+            return nil
+        }
+    }
+}

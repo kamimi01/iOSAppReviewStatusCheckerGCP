b31d7c4059886941eae1089bd744cf030aa77f9f
mika.urakawa <awakaru0122@gmail.com>
2023-04-20 07:10:04 +0900

    feat: create entity of app review to make it easier to generate post message

---
+++ Sources/App/Controllers/AppReviewSubmission.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by mikaurakawa on 2023/04/20.
+//
+
+import Foundation
+
+struct AppReviewSubmission {
+    let name: String
+    let version: String
+    let submittedDate: String
+    let appStoreState: String
+    let appStoreStateEmoji: String
+}

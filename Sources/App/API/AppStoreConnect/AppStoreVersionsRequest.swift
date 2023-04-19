9c1f19d4e22aa709a2bc867d9e64012c7e26b9ef
mika.urakawa <awakaru0122@gmail.com>
2023-04-20 07:47:18 +0900

    feat: create app store versions request

---
+++ Sources/App/API/AppStoreConnect/AppStoreVersionsRequest.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by mikaurakawa on 2023/04/20.
+//
+
+import Foundation
+
+struct AppStoreVersionsRequest: AppStoreConnectRequest {
+    typealias Response = AppStoreVersions
+
+    let method: HttpMethod = .get
+    let path: String
+    let token: String
+
+    var headerFields: [String : String] {
+        return ["Authorization": token]
+    }
+
+    var queryParameters: [String : String]? {
+        var params: [String: String] = [
+            "fields[appStoreVersions]": "appStoreState,versionString",
+            "limit": "1"
+        ]
+
+        return params
+    }
+
+    var body: Data? = nil
+
+    init(appID: String, token: String) {
+        self.path = "/apps/\(appID)/appStoreVersions"
+        self.token = token
+    }
+}

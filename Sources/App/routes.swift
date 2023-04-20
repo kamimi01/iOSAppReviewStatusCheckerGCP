380997666271104f985d7e1dcfbf6db2976cdf5b
mika.urakawa <awakaru0122@gmail.com>
2023-04-21 06:30:12 +0900

    fix: change called method and order

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,24 +2,27 @@ import Vapor
 import JWT
 
 // TODO: 複数アプリのステータスを投稿できるように
-// TODO: アプリ名とバージョンを取得できるように
 // TODO: 期限が切れていないJWTがあったらそれを使用するように
 // TODO: APIではなく、バッチとして実装するように
 func routes(_ app: Application) throws {
-    app.get { req async in
+    app.get("postReviewSubmissionState") { req async in
         let appID = "1673161138"
 
         do {
             // App Store Connectから審査情報を取得する
             let ascController = AppStoreConnectController(app: app, req: req, appIDs: [appID])
-            let reviewSubmissions = try await ascController.requestReviewSubmission()
+            // アプリ名を取得する
+            let apps = try await ascController.requestApps()
+            // バージョンと作成日時、ステータスを取得する
+            let appStoreVersions = try await ascController.requestAppStoreVersions(appID: appID)
 
             // 投稿するメッセージを生成する
             let messageGenerator = MessageGenerator()
             let postMessage = try messageGenerator.generatePostMessage(
-                appID: appID,
-                submittedDate: reviewSubmissions.data.first?.attributes.submittedDate,
-                state: reviewSubmissions.data.first?.attributes.state
+                appName: apps.data.first?.attributes.name,
+                appVersion: appStoreVersions.data.first?.attributes.versionString,
+                createdDate: appStoreVersions.data.first?.attributes.createdDate,
+                state: appStoreVersions.data.first?.attributes.appStoreState
             )
 
             // Slackに投稿する

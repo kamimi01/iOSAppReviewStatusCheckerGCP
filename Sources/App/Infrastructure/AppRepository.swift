7fe3d6c7ecd4e0d3105b0831ea1413d85374fa85
mika.urakawa <awakaru0122@gmail.com>
2023-04-23 18:56:22 +0900

    feat: create AppInfo Repository

---
+++ Sources/App/Infrastructure/AppRepository.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  
+//
+//  Created by mikaurakawa on 2023/04/23.
+//
+
+import Foundation
+
+class AppRepository {
+    // TODO: tokenは引数から無くして、このメソッドの内部で生成するようにしたい
+    func fetch(id: String, token: String) async throws -> AppInfo {
+        let session = Session()
+        let request = AppsRequest(token: token)
+        let result = try await session.send(request)
+
+        var name: String? {
+            for app in result.data where app.id == id {
+                return app.attributes.name
+            }
+            return nil
+        }
+
+        guard let name = name else {
+            throw AppStoreConnectRequestError.notFoundAppName
+        }
+
+        return AppInfo(id: id, name: name)
+    }
+}

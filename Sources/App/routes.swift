97e82f0a8bafbcd7a580c47777cc0fecbe16fc43
mika.urakawa <awakaru0122@gmail.com>
2023-04-25 07:29:10 +0900

    refactor: change caller structure "route"

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,43 +9,13 @@ func routes(_ app: Application) throws {
             throw Abort(.badRequest, reason: "no required path paramter appID or channelID")
         }
 
-        // App Store Connectから審査情報を取得する
-        let ascController = AppStoreConnectController(app: app, req: req)
-        // 投稿するメッセージを生成する
-        let messageGenerator = MessageGenerator()
-        var postMessages = "iOS アプリのステータスをお知らせします🍎\n\n"
-
         for appID in appIDs {
-            do {
-                // アプリ名を取得する
-                let appName = try await ascController.requestApp(appID: appID)
-
-                // バージョンと作成日時、ステータスを取得する
-                let appStoreVersions = try await ascController.requestAppStoreVersions(appID: appID)
-
-                // 投稿するメッセージを生成する
-                let postMessage = try messageGenerator.generatePostMessage(
-                    appName: appName,
-                    appVersion: appStoreVersions.data.first?.attributes.versionString,
-                    createdDate: appStoreVersions.data.first?.attributes.createdDate,
-                    state: appStoreVersions.data.first?.attributes.appStoreState
-                )
-                postMessages += postMessage
-            } catch AppStoreConnectRequestError.cannotGenerateJWT {
-                throw Abort(.unauthorized, reason: "appID is \(appID)\nFailed to generate JWT")
-            } catch {
-                throw Abort(.badRequest, reason: "appID is \(appID)\n\(error.localizedDescription)")
-            }
-        }
-
-        do {
-            // Slackに投稿する
-            let slackController = SlackController()
-            try await slackController.post(to: postAppStoreState.channelID, message: postMessages)
-        } catch SlackRequestError.unexpectedError(let error) {
-            throw Abort(.badRequest, reason: "slack request is bad: \(String(describing: error))")
-        } catch {
-            throw Abort(.badRequest, reason: error.localizedDescription)
+            // TODO: appIDの数だけ投稿されてしまったので修正する
+            let usecase = PostAppStateToSlackUseCase(app: app, req: req)
+            try await usecase.postAppStateToSlack(
+                appID: appID,
+                channelID: postAppStoreState.channelID
+            )
         }
 
         return Response(status: .ok)

23495d5443964480b38fd4e00068a8f0b9b360e7
mika.urakawa <awakaru0122@gmail.com>
2023-04-25 07:16:43 +0900

    fix: fix dto and usecase class

--- Sources/App/Usecase/PostAppStateToSlackDTO.swift
+++ Sources/App/Usecase/PostAppStateToSlackDTO.swift
@@ -9,18 +9,6 @@ import Foundation
 
 struct PostAppStateToSlackDTO {
     let appID: String
-    let appName: String
-    let version: String
-    let createdDateString: String
-    let appState: AppStoreState
-    let appStateEmoji: String
-
-    init(appID: String, appName: String, version: String, createdDate: Date, appState: AppStoreState, appStateEmoji: String) {
-        self.appID = appID
-        self.appName = appName
-        self.version = version
-        self.createdDateString = createdDate.stringFromDate(format: "yyyy/MM/dd HH:mm:ss")
-        self.appState = appState
-        self.appStateEmoji = appStateEmoji
-    }
+    let channelID: String
+    let postMessage: String
 }
--- Sources/App/Usecase/PostAppStateToSlackUseCase.swift
+++ Sources/App/Usecase/PostAppStateToSlackUseCase.swift
@@ -11,30 +11,29 @@ class PostAppStateToSlackUseCase {
     // TODO: リポジトリをDI して使う
     private let appRepository: AppRepository
     private let appStoreVersionRepository: AppStoreVersionRepository
+    private let slackRepository: SlackRepository
 
-    init(appRepository: AppRepository, appStoreVersionRepository: AppStoreVersionRepository) {
+    init(appRepository: AppRepository, appStoreVersionRepository: AppStoreVersionRepository, slackRepository: SlackRepository) {
         self.appRepository = appRepository
         self.appStoreVersionRepository = appStoreVersionRepository
+        self.slackRepository = slackRepository
     }
 
     // TODO: トークンは引数から無くして内部処理に隠蔽したい
-    func postAppStateToSlack(appID: String, token: String) async throws -> PostAppStateToSlackDTO {
+    func postAppStateToSlack(appID: String, channelID: String, token: String) async throws -> PostAppStateToSlackDTO {
         // TODO: AppRepositoryへのfetchとAppStoreStateRepositoryのfetchは同時実行してOKなので、await を外して並行処理を実施できるように修正したい
         let app = try await appRepository.fetch(id: appID, token: token)
 
         let appStoreVersion = try await appStoreVersionRepository.fetch(id: appID, token: token)
 
-        // TODO: 実際にSlack にポストする（Slackポストはリポジトリに実装する）
+        // TODO: メッセージの生成をする
+        let postMessage = "テスト投稿"
+        let slackPostResult = try await slackRepository.post(to: channelID, message: postMessage)
 
-
-        // TODO: DTOはchannelID, messageを返せばいい
         return PostAppStateToSlackDTO(
             appID: appID,
-            appName: app.name,
-            version: appStoreVersion.version,
-            createdDate: appStoreVersion.createdDate,
-            appState: appStoreVersion.appStoreState,
-            appStateEmoji: AppStoreState(rawValue: appStoreVersion.appStoreState.rawValue)!.emoji
+            channelID: channelID,
+            postMessage: postMessage
         )
     }
 }

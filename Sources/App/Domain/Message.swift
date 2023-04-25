365976db51b42cdc52b062dc2a712dea0b99402a
mika.urakawa <awakaru0122@gmail.com>
2023-04-26 07:42:35 +0900

    feat: create message value object and repository

---
+++ Sources/App/Domain/Message.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by mikaurakawa on 2023/04/26.
+//
+
+import Foundation
+
+enum MessageGenerateError: Error {
+    case requiredParametersAreNil((appName: String?, appVersion: String?, createdDate: Date?, state: AppStoreState?))
+}
+
+struct Message {
+    let text: String
+
+    init(appName: String?, appVersion: String?, createdDate: Date?, state: AppStoreState?) throws {
+        guard let appName = appName,
+              let appVersion = appVersion,
+              let createdDate = createdDate,
+              let state = state
+        else { throw MessageGenerateError.requiredParametersAreNil((appName, appVersion, createdDate, state)) }
+
+        let formattedCreatedDateString = createdDate.stringFromDate(format: "yyyy/MM/dd HH:mm:ss")
+
+        let message = """
+        【\(appName)】
+        バージョン：\(appVersion)
+        ステータス：\(state.display)（\(state.rawValue)） \(state.emoji)
+        作成日時：\(formattedCreatedDateString)
+        \n
+        """
+
+        self.text = message
+    }
+}
---
+++ Sources/App/Infrastructure/MessageRepository.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  
+//
+//  Created by mikaurakawa on 2023/04/26.
+//
+
+import Foundation
+
+class MessageRepository {
+    func generateMessageForApp(appInfo: AppInfo, appStoreVersionInfo: AppStoreVersionInfo) throws -> Message {
+        return try Message(
+            appName: appInfo.name,
+            appVersion: appStoreVersionInfo.version,
+            createdDate: appStoreVersionInfo.createdDate,
+            state: appStoreVersionInfo.appStoreState
+        )
+    }
+
+    func generateMessage(messagesForApp: [Message]) -> String {
+        var message: String {
+            var fullMessage = "iOS アプリのステータスをお知らせします 🍎"
+            for messageForApp in messagesForApp {
+                fullMessage += "\n\(messageForApp.text)"
+            }
+            return fullMessage
+        }
+        return message
+    }
+}

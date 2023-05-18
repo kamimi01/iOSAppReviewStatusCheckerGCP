17be7390873af749100a87e5ebd12f770e2b0df7
mika.urakawa <awakaru0122@gmail.com>
2023-05-19 07:01:57 +0900

    fix: use cache of apps request

--- Sources/App/Infrastructure/AppRepositoryImpl.swift
+++ Sources/App/Infrastructure/AppRepositoryImpl.swift
@@ -13,13 +13,26 @@ enum AppStoreConnectRequestError: Error {
 }
 
 class AppRepositoryImpl: AppRepository {
+    private var cache: AppsRequest.Response?
+
     // TODO: tokenは引数から無くして、このメソッドの内部で生成するようにしたい
     func fetch(id: String, token: String, req: Vapor.Request) async throws -> AppName {
-        let request = AppsRequest(token: token)
-        let client = VaporAPIClient(req: req)
-        let result = try await client.request(request)
+        
+        var result: AppsRequest.Response?
+        if let cache = cache {
+            result = cache
+            req.logger.info("use cache of apps request")
+        } else {
+            let request = AppsRequest(token: token)
+            let client = VaporAPIClient(req: req)
+            result = try await client.request(request)
+            cache = result
+            req.logger.info("dont use cache and request apps")
+        }
 
         var name: String? {
+            guard let result = result else { return nil }
+
             for app in result.data where app.id == id {
                 return app.attributes.name
             }
